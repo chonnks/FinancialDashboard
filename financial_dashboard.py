22e2ce7aa67b98d3fe0964d3d45ab180b743d6de
chonnks <136377272+chonnks@users.noreply.github.com>
2023-12-14 22:03:55 -0500

    Add files via upload
    imported all of the downloads needed to visualize the dashboard I had to download bokeh, numpy, and yahoo finance's API.

---
+++ financial_dashboard.py
@@ -0,0 +1,22 @@
+#Bruce Reyes 
+#INST126 
+#This is a financial dashboard that a user can use. I will be using the yfinance API, numpy and bokeh for visualization
+#I was able to attain information about how to use it on their websites
+
+#import all factors needed to run this program
+#rename some imports that will called back in your code to something shorter
+import math
+import datetime as dt
+import numpy as np
+import yfinance as yf
+
+#import all of things needed to visualize the dashboard
+from bokeh.io import curdoc
+from bokeh.plotting import figure 
+from bokeh.layouts import row, column
+from bokeh.models import TextInput, Button, DatePicker, MultiChoice
+
+
+
+
+
